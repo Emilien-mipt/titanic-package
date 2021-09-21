4200f67cf7b10731c3826983828213a21fec62ea
Emin <emin.tagiev@phystech.edu>
2021-09-21 19:25:50 +0300

    Modify train

--- titanic_package/train_pipeline.py
+++ titanic_package/train_pipeline.py
@@ -1,3 +1,5 @@
+import re
+
 import numpy as np
 from config.core import config
 from pipeline import titanic_pipe
@@ -5,11 +7,43 @@ from processing.data_manager import load_dataset, save_pipeline
 from sklearn.model_selection import train_test_split
 
 
+# retain only the first cabin if more than
+# 1 are available per passenger
+def get_first_cabin(row):
+    try:
+        return row.split()[0]
+    except TypeError:
+        return np.nan
+
+
+def get_title(passenger):
+    line = passenger
+    if re.search("Mrs", line):
+        return "Mrs"
+    elif re.search("Mr", line):
+        return "Mr"
+    elif re.search("Miss", line):
+        return "Miss"
+    elif re.search("Master", line):
+        return "Master"
+    else:
+        return "Other"
+
+
 def run_training() -> None:
     """Train the model."""
 
     # read training data
     data = load_dataset(file_name=config.app_config.training_data_file)
+
+    data["Cabin"] = data["Cabin"].apply(get_first_cabin)
+    data["Title"] = data["Name"].apply(get_title)
+
+    # cast numerical variables as floats
+    data["Fare"] = data["Fare"].astype("float")
+    data["Age"] = data["Age"].astype("float")
+
+    data.drop(labels=config.model_config.variables_to_drop, axis=1, inplace=True)
 
     # divide train and test
     X_train, X_test, y_train, y_test = train_test_split(
