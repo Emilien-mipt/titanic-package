deb4bfbd124891cea192eb43fb3054f75d8fb644
emin <emin.tagiev@phystech.edu>
2025-05-21 17:48:02 +0300

    Test for get_title function

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,6 +1,10 @@
+import numpy as np
+
 from titanic_model.processing.validation import get_first_cabin
 from titanic_model.processing.validation import get_title
 from titanic_model.processing.validation import drop_na_inputs
+
+from titanic_model.config.core import config
 
 import pandas as pd
 
@@ -15,3 +19,17 @@ def test_get_first_cabin(sample_input_data):
     assert sample_input_data["Cabin"].iat[12] == "B"
     assert sample_input_data["Cabin"].iat[14] == "E"
     assert pd.isna(sample_input_data["Cabin"].iat[0])
+
+
+def test_get_title(sample_input_data):
+    sample_input_data["Title"] = sample_input_data["Name"].apply(get_title)
+    assert sample_input_data["Title"].iat[12] == 'Mrs'
+    assert sample_input_data["Title"].iat[13] == 'Mr'
+    assert sample_input_data["Title"].iat[36] == 'Miss'
+    assert sample_input_data["Title"].iat[55] == 'Master'
+    assert sample_input_data["Title"].iat[88] == 'Other'
+
+
+
+
+
