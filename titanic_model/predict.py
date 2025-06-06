e0b9a583906ceae3b6f1797790ee2350f6bacdc1
emin <emin.tagiev@phystech.edu>
2025-06-07 02:21:04 +0300

    Fix pipe name and add prediction on the test set

--- titanic_model/predict.py
+++ titanic_model/predict.py
@@ -4,11 +4,11 @@ import pandas as pd
 
 from titanic_model import __version__ as _version
 from titanic_model.config.core import config
-from titanic_model.processing.data_manager import load_pipeline
+from titanic_model.processing.data_manager import load_dataset, load_pipeline
 from titanic_model.processing.validation import validate_inputs
 
 pipeline_file_name = f"{config.app_config.pipeline_save_file}{_version}.pkl"
-_price_pipe = load_pipeline(file_name=pipeline_file_name)
+_titanic_pipe = load_pipeline(file_name=pipeline_file_name)
 
 
 def make_prediction(
@@ -22,11 +22,16 @@ def make_prediction(
 
     results: t.Dict[str, t.Any] = {"preds": None, "probs": None, "version": _version, "errors": errors}
 
-    preds = _price_pipe.predict(X=validated_data[config.model_config_params.features])
-    probs = _price_pipe.predict_proba(X=validated_data[config.model_config_params.features])[:, 1]
+    preds = _titanic_pipe.predict(X=validated_data[config.model_config_params.features])
+    probs = _titanic_pipe.predict_proba(X=validated_data[config.model_config_params.features])[:, 1]
 
     # Fill the results dict
     results["preds"] = [pred for pred in preds]
     results["probs"] = [prob for prob in probs]
 
     return results
+
+
+if __name__ == "__main__":
+    test_data = load_dataset(file_name=config.app_config.test_data_file)
+    result = make_prediction(input_data=test_data)
